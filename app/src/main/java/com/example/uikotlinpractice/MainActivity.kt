c5d30fb7ed94c4a45f5242e93037de5a0f07da2e
studentDG <daegu776@gmail.com>
2021-10-28 19:54:18 +0900

    코틀린에서 저장 해둔 입력값(get)을 텍스트뷰에 반영(set)

--- app/src/main/java/com/example/uikotlinpractice/MainActivity.kt
+++ app/src/main/java/com/example/uikotlinpractice/MainActivity.kt
@@ -9,6 +9,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//      확인버튼이 눌리면? 이벤트를 달아주자.
 
+        okBtn.setOnClickListener {
+//            입력된 내용? : contentEdt 의 text 의 값 조회(get) 변수에 저장 해두자.
+
+            val inputContent = contentEdt.text.toString()
+
+//            저장해둔 입력값을 => 텍스트뷰의 내용으로 반영(set).
+
+            resultTxt.text = inputContent
+
+        }
     }
 }
